bc1ed78d1cd8fe9e58ad33c424828984a12e3dca
dhiebert7251 <dan.hiebert@sbcglobal.net>
2024-02-06 10:55:17 -0600

    ps4 and auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,16 +5,17 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-
 import frc.robot.subsystems.SysIDSubsystem;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -27,14 +28,24 @@ public class RobotContainer {
   // The robot's subsystems
   private final SysIDSubsystem m_sysID = new SysIDSubsystem();
 
+  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+
   // The driver's controller
-  XboxController m_driverController = new XboxController(OperatorConstants.kDriverControllerPort);
+  //XboxController m_driverController = new XboxController(OperatorConstants.kDriverControllerPort);
+  PS4Controller m_driverController = new PS4Controller(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     
     // Configure the trigger bindings
     configureBindings();
+
+    autoChooser.setDefaultOption("Do Nothing", null);
+    autoChooser.addOption("Quasi Fwd", m_sysID.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+    autoChooser.addOption("Quasi Rev", m_sysID.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+    autoChooser.addOption("Dynamic Fwd", m_sysID.sysIdDynamic(SysIdRoutine.Direction.kForward));
+    autoChooser.addOption("Dynamic Rev", m_sysID.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   /**
@@ -50,13 +61,13 @@ public class RobotContainer {
     //SYS ID
     // Bind full set of SysId routine tests to buttons; a complete routine should run each of these
     // once.
-    new JoystickButton(m_driverController, Button.kA.value)
+    new JoystickButton(m_driverController, Button.kCross.value)
       .onTrue(new InstantCommand(() -> m_sysID.sysIdQuasistatic(SysIdRoutine.Direction.kForward)));
-    new JoystickButton(m_driverController, Button.kB.value)
+    new JoystickButton(m_driverController, Button.kCircle.value)
       .onTrue(new InstantCommand(() -> m_sysID.sysIdQuasistatic(SysIdRoutine.Direction.kReverse)));
-    new JoystickButton(m_driverController, Button.kX.value)
+    new JoystickButton(m_driverController, Button.kSquare.value)
       .onTrue(new InstantCommand(() -> m_sysID.sysIdDynamic(SysIdRoutine.Direction.kForward)));
-    new JoystickButton(m_driverController, Button.kY.value)
+    new JoystickButton(m_driverController, Button.kTriangle.value)
       .onTrue(new InstantCommand(() -> m_sysID.sysIdDynamic(SysIdRoutine.Direction.kReverse)));
 
   }
@@ -67,7 +78,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return null;
+    return autoChooser.getSelected();
+  
   }
 }
--- src/main/java/frc/robot/subsystems/SysIDSubsystem.java
+++ src/main/java/frc/robot/subsystems/SysIDSubsystem.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+
 import frc.robot.Constants.PhysicalConstants;
 import frc.robot.Constants.DriveConstants;
 
